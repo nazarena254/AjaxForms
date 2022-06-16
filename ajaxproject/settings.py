87ffeafa743fb021f39673fa018e39fb44551abe
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-17 00:00:07 +0300

    update readme

--- ajaxproject/settings.py
+++ ajaxproject/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 MODE=config("MODE", default="dev")
 DISABLE_COLLECTSTATIC=config('DISABLE_COLLECTSTATIC')
 ALLOWED_HOSTS = ["*"]
