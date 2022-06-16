26b78fc91bea3f30c1165ea51bece454bea05fe5
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-17 00:44:42 +0300

    cadd abckground image

--- ajaxproject/settings.py
+++ ajaxproject/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS=[
-        os.path.join(BASE_DIR, 'static')
-    ]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS=[
+#         os.path.join(BASE_DIR, 'static')
+#     ]
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=[
