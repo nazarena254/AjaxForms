9106957a5f7e5f9cd5fd5c8b030ea866f08d4c19
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-17 12:45:23 +0300

    add  afunction to check if the nickname exists

--- ajaxapp/views.py
+++ ajaxapp/views.py
@@ -36,6 +36,20 @@ def postFriend(request):
     return JsonResponse({"error": ""}, status=400)
 
 
+def checkNickName(request):
+    # request should be ajax and method should be GET.
+    if request.is_ajax and request.method == "GET":
+        # get the nick name from the client side.
+        nick_name = request.GET.get("nick_name", None)
+        # check for the nick name in the database.
+        if Friend.objects.filter(nick_name = nick_name).exists():
+            # if nick_name found return not valid new friend
+            return JsonResponse({"valid":False}, status = 200)
+        else:
+            # if nick_name not found, then user can create a new friend.
+            return JsonResponse({"valid":True}, status = 200)
+
+    return JsonResponse({}, status = 400)
 
 
 
