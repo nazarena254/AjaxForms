34dbfa8e4862f571f91f33ac3fd9ee648bb0a0be
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-17 12:48:11 +0300

    add url for checkNickName view fn

--- ajaxapp/urls.py
+++ ajaxapp/urls.py
@@ -2,11 +2,15 @@ from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from ajaxapp.views import (
+    checkNickName
+)
 #from .views import index
 
 urlpatterns=[
     path('',views.indexView , name="index"),
-    path('post/ajax/friend',views.postFriend, name="post_friend")
+    path('post/ajax/friend',views.postFriend, name="post_friend"),
+    path('get/ajax/validate/nickname', checkNickName, name = "validate_nickname"),
 ]
 
 if settings.DEBUG:
